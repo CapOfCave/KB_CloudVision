d1fe730d5f746561ba66940dbf692c2c627e52ff
Linus <linus.ricker@gmail.com>
2020-04-10 16:41:38 +0200

    capa 1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
+using Google.Cloud.Vision.V1;
+using System;
+using System.Linq;
+
 namespace KB_CloudVision
 {
     class Program
     {
-        static void Main(string[] args)
+        static readonly string s_usage = @"dotnet run image-file
+
+    Use the Google Cloud Vision API to detect faces in the image.
+    Writes an output file called image-file.faces.
+    ";
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lars!");
+            if (args.Length < 1)
+            {
+                Console.WriteLine(s_usage);
+                return;
+            }
+
+            var client = ImageAnnotatorClient.Create();
+            var response = client.DetectFaces(Image.FromFile(args[0]));
+
+            int numberOfFacesFound = 0;
+            using (var image = System.Drawing.Image.FromFile(args[0]))
+            using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(image))
+            {
+                var cyanPen = new System.Drawing.Pen(System.Drawing.Color.Cyan, 3);
+                foreach (var annotation in response)
+                {
+                    g.DrawPolygon(cyanPen, annotation.BoundingPoly.Vertices.Select(
+                        (vertex) => new System.Drawing.Point(vertex.X, vertex.Y)).ToArray());
+                    // ...
+                }
+                // ...
+            }
+            // ...
         }
     }
 }
