c75e7c8a2bbfa1188c0de11afebc6783bc80ca96
CapOfCave <lars.kecker2009@gmail.com>
2020-04-10 15:22:02 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KB_CloudVision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
